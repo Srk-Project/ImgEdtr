a72cffed2df7538d9c6d44dd4f329a646b648559
Sidharth Priyadarshi <imsidharthpriyadarshi@gmail.com>
2023-06-01 12:17:48 +0530

    add binding

--- app/src/main/java/com/example/simpleeditor/MainActivity.kt
+++ app/src/main/java/com/example/simpleeditor/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.simpleeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simpleeditor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+
     }
 }
