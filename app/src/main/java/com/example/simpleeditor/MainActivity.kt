3c7a6735b40223a5a1356683cbb6ace3f86fa5d1
2coolcoder <irupankarpodder@gmail.com>
2023-06-02 11:19:26 +0530

    Binding, python

--- app/src/main/java/com/example/simpleeditor/MainActivity.kt
+++ app/src/main/java/com/example/simpleeditor/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.simpleeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.chaquo.python.PyObject
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+import com.example.simpleeditor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding= ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
+        if(!Python.isStarted())
+            Python.start(AndroidPlatform(this))
+        val py = Python.getInstance()
+        val file = py.getModule("file")
+        val j: PyObject? = file.callAttr("strstr","Iammonkey")
+        Toast.makeText(this, j.toString(), Toast.LENGTH_SHORT).show()
     }
+
+
 }
